0c3a711ee8c9bbd7a30bd4f3d04bd613d87d7925
Viateur <akimanaviateur94@gmail.com>
2023-11-21 22:01:50 +0100

    middlewares morgan

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const logger = require("./logger");
-const authorize = require("./authorize")
+const {people} = require("./data")
+app.use(express.static("./method-public"))
+// // const morgan = require('morgan')
+// // const logger = require("./logger");
+// // const authorize = require("./authorize")
 
-// app.use(logger);
-// app.use(authorize);
-app.use(morgan('tiny'))
-app.get("/",(req,res)=>{
-  res.send("Home page");
-})
-app.get("/about",(req,res)=>{
-  res.send("This about page")
+// // // app.use(logger);
+// // // app.use(authorize);
+// // app.use(morgan('tiny'))
+// app.get("/",(req,res)=>{
+//   res.send("Home page");
+// })
+// app.get("/about",(req,res)=>{
+//   res.send("This about page")
+// })
+
+app.get('/api/people',(req,res)=>{
+  res.status(200).send({success:true,data:people});
 })
 
 app.listen(5001,()=>{
