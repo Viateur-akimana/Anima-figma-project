6fd7ed4a11987bdf20a3bab9f2d1dffd3a01e8dd
Viateur <akimanaviateur94@gmail.com>
2023-11-21 06:15:49 +0100

    operations on the data

--- 2-express/app.js
+++ 2-express/app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const {products} = require("./data");
 const app = express();
 app.get("/",(req,res)=>{
-  res.json(products);
+  res.send("<h1>Welmcome to homepage</h1><a href='/api/product'> products</a>")
 })
-
+app.get("/api/product",(req,res)=>{
+  const newProduct = products.map((product)=>{
+  
+    return product;
+  })
+  res.json(newProduct);
+})
+app.get("/api/product/1",(req,res)=>{
+  const singleProduct = products.find((product) => product.id === 1)
+  res.json(singleProduct);
+})
+app.get("/api/product/:productID",(req,res)=>{
+  const {productID} = req.params;
+  const singleProduct = products.find((product) => product.id == productID)
+  if(!singleProduct)
+  return res.status(404).json("Product doesn,t exist");
+  res.json(singleProduct);
+})
 app.get("/about",(req,res)=>{
   res.status(200).send("You are welcome back on the about page ")
 })
@@ -13,5 +30,5 @@ app.get("*",(req,res)=>{
 })
 
 app.listen(5000,(req,res)=>{
-  console.log("Server on running on port 5000...");
+  console.log("Server on running on port 50000...");
 })
