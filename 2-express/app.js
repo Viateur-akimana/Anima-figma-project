9108817823c829aad37f849079274bedfff85fe8
Viateur <akimanaviateur94@gmail.com>
2023-11-22 19:10:49 +0100

    expressjs

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,33 +1,59 @@
 const express = require('express');
 const app = express();
-const {people} = require("./data");
-app.use(express.urlencoded({extended:true}));
+const { people } = require("./data");
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./method-public"));
 
-// // const morgan = require('morgan')
-// // const logger = require("./logger");
-// // const authorize = require("./authorize")
-
-// // // app.use(logger);
-// // // app.use(authorize);
-// // app.use(morgan('tiny'))
-// app.get("/",(req,res)=>{
+// app.get('/', (req, res) => {
 //   res.send("Home page");
-// })
-// app.get("/about",(req,res)=>{
-//   res.send("This about page")
-// })
+// });
 
-app.get('/api/people',(req,res)=>{
-  res.status(200).send({success:true,data:people});
-})
-app.post('/login',(req,res)=>{
-  const {name} = req.body;
-  if(name){
-    res.status(200).send(`Welcome ${name}`)
+// app.get('/about', (req, res) => {
+//   res.send("This is the about page");
+// });
+
+app.get('/api/people', (req, res) => {
+  res.status(200).send({ success: true, data: people });
+});
+
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
   }
-  res.status(404).send("Please provide credentials");
+  return res.status(404).send("Please provide credentials");
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(404).json({ success: false, msg: "Please provide the name" });
+  }
+  res.status(200).json({ success: true, person: name });
+});
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(404).json({ success: false, msg: "Please provide the name" });
+  }
+  res.status(200).json([...people,name]);
+});
+app.put("/api/people/:id",(req,res)=>{
+  const {id} = req.params;
+  const {name} = req.body;
+  const person = people.find((person)=> person.id === Number(id))
+  if(!person){
+    res.status(404).json({success:false, msg:"No peson with search id"})
+  }
+  const newPerson = people.map((person)=>{
+    if(person.id === Number(id)){
+      person.name = name;
+}
+  return person;
+  })
+  res.status(200).send({success:true,data:newPerson});
 })
-app.listen(5001,()=>{
-  console.log("THe server is runninfg on port 5000..");
-})
+app.listen(5001, () => {
+  console.log("The server is running on port 5001...");
+});
